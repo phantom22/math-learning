c55aa3ed8aa4ad7fbc888ec8f90a33752902dd18
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2022-11-10 11:44:43 +0100

    .

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-const input = document.getElementById("input");
-if (!(input instanceof HTMLTextAreaElement))
+const _input = document.getElementById("input"), _inputTitle = document.getElementById("input-title"), _prompt = document.getElementById("prompt"), _promptTitle = document.getElementById("prompt-title");
+if (!(_input instanceof HTMLTextAreaElement))
     throw "Couldn't find textarea#input";
-input.value = "";
-input.addEventListener("input", (e) => {
+if (_inputTitle === null)
+    throw "Couldn't find #input-title!";
+if (_prompt === null)
+    throw "Couldn't find #prompt!";
+if (_promptTitle === null)
+    throw "Couldn't find #prompt-title!";
+document.body.addEventListener("keypress", (e) => {
+    if (e.key === "Enter")
+        answerQuestion();
+});
+_input.value = "";
+_input.addEventListener("input", (e) => {
     // @ts-ignore
     let v = e.target.value;
     v = v.replaceAll(/\s/g, "");
     v = v.replaceAll(/v|V/g, "√");
+    v = v.replaceAll(">=", "≥");
+    v = v.replaceAll("<=", "≤");
     //v = v.replaceAll("p","π");
     // @ts-ignore
     e.target.value = v;
 });
 const categories = [
-    //  ###################
-    //  ## TRIGONOMETRIA ##
-    //  ###################
+    //  #######################
+    //  ## TRIGONOMETRIA 0-9 ##
+    //  #######################
     label(labelName("Sia x=0°", "Sia x=360°", "Sia x=2π"), question("cos(x) = ?", "1"), question("sin(x) = ?", "0"), question("tan(x) = ?", "0")),
     label(labelName("Sia x=30°", "Sia x=π/6"), question("cos(x) = ?", "√3/2"), question("sin(x) = ?", "1/2"), question("tan(x) = ?", "√3")),
     label(question("Sia x=45°", "Sia x=π/4"), question("cos(x) = ?", "√2/2"), question("sin(x) = ?", "√2/2"), question("tan(x) = ?", "1")),
@@ -24,17 +36,20 @@ const categories = [
     label(labelName("Sia x=135°", "Sia x=3/4π", "Sia x=90°+45°"), question("cos(x) = ?", "-√2/2"), question("sin(x) = ?", "√2/2"), question("tan(x) = ?", "-1")),
     label(labelName("Sia x=150°", "Sia x=5/6π", "Sia x=180°-30°"), question("cos(x) = ?", "-√3/2"), question("sin(x) = ?", "1/2"), question("tan(x) = ?", "-√3/3")),
     label(labelName("Sia x=180°", "Sia x=π"), question("cos(x) = ?", "-1"), question("sin(x) = ?", "0"), question("tan(x) = ?", "0")),
-    label(labelName("Sia x=270°", "Sia x=3π"), question("cos(x) = ?", "0"), question("sin(x) = ?", "-1"), question("tan(x) = ?", "-")),
-    //  #####################
-    //  ## LIMITI NOTEVOLI ##
-    //  #####################
-    label(labelName("A quanto tende il seguente limite per x->0"), question("sin(x)/x = ?", "1"), question("(1-cos(x))/x^2 = ?", "1/2"), question("ln(1+x)/x = ?", "1"), question("tan(x)/x = ?", "1"), question("(a^x-1)/x = ?", "ln(a)"), question("(e^x-1)/x = ?", "1"), question("arctan(x)/x = ?", "1"), question("arcsin(x)/x = ?", "1")),
+    label(labelName("Sia x=270°", "Sia x=3/4π"), question("cos(x) = ?", "0"), question("sin(x) = ?", "-1"), question("tan(x) = ?", "-")),
+    //  ###########################
+    //  ## LIMITI NOTEVOLI 10-11 ##
+    //  ###########################
+    label(labelName("A quanto tende il seguente limite per x->0"), question("sin(x)/x = ?", "1"), question("sin(x) = ?", "x"), question("(1-cos(x))/x^2 = ?", "1/2"), question("ln(1+x)/x = ?", "1"), question("ln(1+x) = ?", "x"), question("tan(x)/x = ?", "1"), question("tan(x) = ?", "x"), question("(a^x-1)/x = ?", "ln(a)"), question("(e^x-1)/x = ?", "1"), question("(e^x-1) = ?", "x"), question("arctan(x)/x = ?", "1"), question("arctan(x) = ?", "x"), question("arcsin(x)/x = ?", "1"), question("arcsin(x) = ?", "x"), question("(x->0+) xln(x) = ?", "0")),
     label(labelName("A quanto tende il seguente limite per x->infinito"), question("(1+1/x)^x = ?", "e"), question("(1+a/x)^x = ?", "e^a"), question("(1+x)^-x = ?", "e")),
-    label(labelName("A quanto tende il seguente limite per x->0+"), question("xln(x) = ?", "0")),
-    //  ###############################
-    //  ## IDENTITA' TRIGONOMETRICHE ##
-    //  ###############################
-    label(labelName("Identità trigonometriche; omettere l'argomento delle funzioni"), question("tan = ?", "sin/cos"), question("tan^-1 = ?", "sin/cos"), question("sec = ?", "1/cos"), question("cosec = ?", "1/sin"), question("cot = ?", "1/tan"), question("sin^2 + cos^2 = ?", "1"), question("1 - cos^2 = ?", "sin^2"), question("1 - sin^2 = ?", "cos^2"), question("sin^2 = ?", "1-cos^2"), question("cos^2 = ?", "1-sin^2"))
+    //  ##################################
+    //  ## IDENTITA' TRIGONOMETRICHE 12 ##
+    //  ##################################
+    label(labelName("Identità trigonometriche"), question("tan = ?", "sin/cos"), question("tan^-1 = ?", "cos/sin"), question("sec = ?", "1/cos"), question("cosec = ?", "1/sin"), question("cot = ?", "1/tan"), question("sin^2 + cos^2 = ?", "1"), question("1 - cos^2 = ?", "sin^2"), question("1 - sin^2 = ?", "cos^2"), question("sin^2 = ?", "1-cos^2"), question("cos^2 = ?", "1-sin^2")),
+    // ###################
+    // ## SERIE NOTE 13 ##
+    // ###################
+    label(labelName("Serie note"), question("Σq^a, diverge per a = ?", "a≥1"), question("Σq^a, converge per a = ?", "-1<a<1"), question("Σq^a, a cosa converge se -1<a<1", "1/(1-q)"), question("Σq^a, è indeterminata per a = ?", "a≤-1"), question("Σ1/n^a diverge per a = ?", "a≤1"), question("Σ1/n^a converge per a = ?", "a>1"), question("Σ1/n(ln(n))^a diverge per a = ?", "a≤1"), question("Σ1/n(ln(n))^a converge per a = ?", "a>1"))
 ];
 // after textarea is evaluated the input is nicely formatted
 function question(query, answer) {
@@ -47,3 +62,55 @@ function label(names, ...questions) {
     return [names, ...questions];
 }
 //alert("Indicare la 'radice quadrata' con 'v'\nIndicare 'Non Esiste' con '-'")
+let _c = -1, _n = -1, _q = -1;
+function pickQuestion() {
+    function section(v) {
+        let o = -1;
+        if (v <= 9)
+            o = 0;
+        else if (v <= 11)
+            o = 1;
+        else if (v === 12)
+            o = 2;
+        else if (v === 13)
+            o = 3;
+        return o;
+    }
+    let cat = ~~(Math.random() * categories.length), _sec = ((v) => {
+        let o = -1;
+        if (v <= 9)
+            o = 0;
+        else if (v <= 12)
+            o = 1;
+        else if (v === 13)
+            o = 2;
+        else if (v === 14)
+            o = 3;
+        return o;
+    })(_c);
+    while (section(cat) === section(_c)) {
+        cat = ~~(Math.random() * categories.length);
+    }
+    _c = cat;
+    _n = ~~(Math.random() * categories[_c][0].length);
+    _q = ~~(Math.random() * categories[_c].length - 1) + 1;
+    updateQuestionUI();
+}
+function answerQuestion() {
+    const correctAnswer = categories[_c][_q][1], userAnswer = _input.value;
+    console.log({ userAnswer, correctAnswer });
+    if (correctAnswer === userAnswer) {
+        pickQuestion();
+        _input.value = "";
+        _inputTitle.textContent = "Risposta";
+    }
+    else {
+        _inputTitle.textContent = "Risposta = " + correctAnswer;
+    }
+}
+function updateQuestionUI() {
+    const cat = categories[_c];
+    _promptTitle.textContent = cat[0][_n];
+    _prompt.textContent = cat[_q][0];
+}
+pickQuestion();
--- main.ts
+++ main.ts
@@ -1,24 +1,33 @@
-const _input = document.getElementById("input"),
+const _input = <HTMLTextAreaElement>document.getElementById("input"),
       _inputTitle = document.getElementById("input-title"),
       _prompt = document.getElementById("prompt"),
       _promptTitle = document.getElementById("prompt-title");
 if (!(_input instanceof HTMLTextAreaElement)) throw "Couldn't find textarea#input";
+if (_inputTitle===null) throw "Couldn't find #input-title!";
+if (_prompt===null) throw "Couldn't find #prompt!";
+if (_promptTitle===null) throw "Couldn't find #prompt-title!";
+
+document.body.addEventListener("keypress",(e)=>{
+    if (e.key==="Enter") answerQuestion();
+});
 
 _input.value = "";
 _input.addEventListener("input",(e) => {
     // @ts-ignore
     let v = e.target.value;
     v = v.replaceAll(/\s/g,"");
-    v = v.replaceAll(/v|V/g,"√");1
+    v = v.replaceAll(/v|V/g,"√");
+    v = v.replaceAll(">=","≥");
+    v = v.replaceAll("<=","≤");
     //v = v.replaceAll("p","π");
     // @ts-ignore
     e.target.value = v;
 });
 
 const categories = [
-    //  ###################
-    //  ## TRIGONOMETRIA ##
-    //  ###################
+    //  #######################
+    //  ## TRIGONOMETRIA 0-9 ##
+    //  #######################
     label(
         labelName("Sia x=0°","Sia x=360°","Sia x=2π"),
         question("cos(x) = ?","1"),
@@ -74,24 +83,31 @@ const categories = [
         question("tan(x) = ?","0")
     ),
     label(
-        labelName("Sia x=270°","Sia x=3π"),
+        labelName("Sia x=270°","Sia x=3/4π"),
         question("cos(x) = ?","0"),
         question("sin(x) = ?","-1"),
         question("tan(x) = ?","-")
     ),
-    //  #####################
-    //  ## LIMITI NOTEVOLI ##
-    //  #####################
+    //  ###########################
+    //  ## LIMITI NOTEVOLI 10-11 ##
+    //  ###########################
     label(
         labelName("A quanto tende il seguente limite per x->0"),
         question("sin(x)/x = ?","1"),
+        question("sin(x) = ?","x"),
         question("(1-cos(x))/x^2 = ?","1/2"),
         question("ln(1+x)/x = ?","1"),
+        question("ln(1+x) = ?","x"),
         question("tan(x)/x = ?","1"),
+        question("tan(x) = ?","x"),
         question("(a^x-1)/x = ?","ln(a)"),
         question("(e^x-1)/x = ?","1"),
+        question("(e^x-1) = ?","x"),
         question("arctan(x)/x = ?","1"),
+        question("arctan(x) = ?","x"),
         question("arcsin(x)/x = ?","1"),
+        question("arcsin(x) = ?","x"),
+        question("(x->0+) xln(x) = ?","0"),
     ),
     label(
         labelName("A quanto tende il seguente limite per x->infinito"),
@@ -99,17 +115,13 @@ const categories = [
         question("(1+a/x)^x = ?","e^a"),
         question("(1+x)^-x = ?","e"),
     ),
+    //  ##################################
+    //  ## IDENTITA' TRIGONOMETRICHE 12 ##
+    //  ##################################
     label(
-        labelName("A quanto tende il seguente limite per x->0+"),
-        question("xln(x) = ?","0"),
-    ),
-    //  ###############################
-    //  ## IDENTITA' TRIGONOMETRICHE ##
-    //  ###############################
-    label(
-        labelName("Identità trigonometriche; omettere l'argomento delle funzioni"),
+        labelName("Identità trigonometriche"),
         question("tan = ?","sin/cos"),
-        question("tan^-1 = ?","sin/cos"),
+        question("tan^-1 = ?","cos/sin"),
         question("sec = ?","1/cos"),
         question("cosec = ?","1/sin"),
         question("cot = ?","1/tan"),
@@ -118,9 +130,22 @@ const categories = [
         question("1 - sin^2 = ?","cos^2"),
         question("sin^2 = ?","1-cos^2"),
         question("cos^2 = ?","1-sin^2"),
-
+    ),
+    // ###################
+    // ## SERIE NOTE 13 ##
+    // ###################
+    label(
+        labelName("Serie note"),
+        question("Σq^a, diverge per a = ?","a≥1"),
+        question("Σq^a, converge per a = ?","-1<a<1"),
+        question("Σq^a, a cosa converge se -1<a<1","1/(1-q)"),
+        question("Σq^a, è indeterminata per a = ?","a≤-1"),
+        question("Σ1/n^a diverge per a = ?","a≤1"),
+        question("Σ1/n^a converge per a = ?","a>1"),
+        question("Σ1/n(ln(n))^a diverge per a = ?","a≤1"),
+        question("Σ1/n(ln(n))^a converge per a = ?","a>1"),
     )
-]
+];
 
 // after textarea is evaluated the input is nicely formatted
 function question(query:string,answer:string) {
@@ -136,3 +161,55 @@ function label(names:string[],...questions:string[][]){
 }
 
 //alert("Indicare la 'radice quadrata' con 'v'\nIndicare 'Non Esiste' con '-'")
+let _c = -1,
+    _n = -1,
+    _q = -1;
+function pickQuestion() {
+    function section(v) {
+        let o = -1;
+        if (v<=9) o = 0;
+        else if (v<=11) o = 1;
+        else if (v===12) o = 2;
+        else if (v===13) o = 3;
+        return o;
+    }
+    let cat = ~~(Math.random() * categories.length),
+        _sec = ((v) => {
+            let o = -1;
+            if (v<=9) o = 0;
+            else if (v<=12) o = 1;
+            else if (v===13) o = 2;
+            else if (v===14) o = 3;
+            return o;
+        })(_c);
+    while (section(cat)===section(_c)) {
+        cat = ~~(Math.random() * categories.length);
+    }
+    _c = cat;
+    _n = ~~(Math.random() * categories[_c][0].length);
+
+    _q = ~~(Math.random() * categories[_c].length - 1) + 1;
+    updateQuestionUI();
+}
+function answerQuestion() {
+    const correctAnswer = categories[_c][_q][1],
+          userAnswer = _input.value;
+
+          console.log({userAnswer,correctAnswer});
+    if (correctAnswer===userAnswer) {
+        pickQuestion();
+        _input.value = "";
+        _inputTitle.textContent = "Risposta";
+    }
+    else {
+        _inputTitle.textContent = "Risposta = " + correctAnswer;
+    }
+
+}
+function updateQuestionUI() {
+    const cat = categories[_c];
+
+    _promptTitle.textContent = cat[0][_n];
+    _prompt.textContent = cat[_q][0];
+}
+pickQuestion();
