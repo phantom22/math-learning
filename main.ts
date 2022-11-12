71f758df120d96602e5c2602e7b8bed711dbd2a9
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2022-11-12 21:18:55 +0100

    .

--- main.ts
+++ main.ts
@@ -13,7 +13,6 @@ window.addEventListener("load", () => {
         if (e.key==="Enter") answerQuestion();
     });
 
-    _input.value = "";
     _input.addEventListener("input",(e) => {
         // @ts-ignore
         let v = e.target.value;
@@ -32,8 +31,8 @@ window.addEventListener("load", () => {
         category:string,
         questions: {
             [questionIndex:number]: {
-                prompt: string;
-                promptMathJax?:[mathJax:string,prompt:string];
+                prompt?: string;
+                promptMathJax?:{equation:string,prompt:string};
                 answer: string | string[];
                 answerMathJax?: string;
             }
@@ -94,65 +93,66 @@ window.addEventListener("load", () => {
         {
             category:"Identità trigonometriche (ignorare l'argomento)",
             questions: [
-                { prompt:"tan", answer:"sin/cos"},
-                { prompt:"tan^-1", answer:["cos/sin","1/tan"]},
-                { prompt:"cot", answer:["cos/sin","1/tan"]},
-                { prompt:"sec", answer:"1/cos"},
-                { prompt:"cosec", answer:"1/sin"},
-                { prompt:"sin^2 + cos^2", answer:"1"},
-                { prompt:"1 - cos^2", answer:"sin^2"},
-                { prompt:"1 - sin^2", answer:"cos^2"},
-                { prompt:"sin^2", answer:"1-cos^2"},
-                { prompt:"cos^2", answer:"1-sin^2"},
+                { prompt:"tan", promptMathJax:{equation:"\\tan x",prompt:"uguale a"}, answer:"sin/cos"},
+                { prompt:"tan^-1", promptMathJax:{equation:"\\tan^{-1} x",prompt:"uguale a"}, answer:["cos/sin","1/tan"]},
+                { prompt:"cot", promptMathJax:{equation:"\\cot x",prompt:"uguale a"}, answer:["cos/sin","1/tan"]},
+                { prompt:"sec", promptMathJax:{equation:"\\sec x",prompt:"uguale a"}, answer:"1/cos"},
+                { prompt:"cosec", promptMathJax:{equation:"\\csc x",prompt:"uguale a"}, answer:"1/sin"},
+                { prompt:"sin^2 + cos^2", promptMathJax:{equation:"\\sin^2 x + \\cos^2 x",prompt:"uguale a"}, answer:"1"},
+                { prompt:"1 - cos^2", promptMathJax:{equation:"1 - \\cos^2 x",prompt:"uguale a"},answer:"sin^2"},
+                { prompt:"1 - sin^2", promptMathJax:{equation:"1 - \\sin^2 x",prompt:"uguale a"},answer:"cos^2"},
+                { prompt:"sin^2", promptMathJax:{equation:"\\sin^2 x",prompt:"uguale a"}, answer:"1-cos^2"},
+                { prompt:"cos^2", promptMathJax:{equation:"\\cos^2 x",prompt:"uguale a"},answer:"1-sin^2"},
             ]
         },
         {
         category:"Limiti notevoli",
             questions:[
-                { prompt:"per x->0, Lim sin(x)/x",  answer:"1" },
-                { prompt:"per x->0, Lim ln(1+x)/x", answer:"1" },
-                { prompt:"per x->0, Lim tan(x)/x", answer:"1" },
-                { prompt:"per x->0, Lim (e^x-1)/x", answer:"1" },
-                { prompt:"per x->0, Lim arctan(x)/x", answer:"1" },
-                { prompt:"per x->0, Lim arcsin(x)/x", answer:"1" },
+                { prompt:"per x->0, Lim sin(x)/x", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} {\\sin x \\over x}",prompt:"uguale a"}, answer:"1" },
+                { prompt:"per x->0, Lim ln(1+x)/x", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} { \\ln (1+x) \\over x }",prompt:"uguale a"}, answer:"1" },
 
-                { prompt:"per x->0, Lim sin(x)", answer:"x" },
-                { prompt:"per x->0, Lim ln(1+x)", answer:"x" },
-                { prompt:"per x->0, Lim tan(x)", answer:"x" },
-                { prompt:"per x->0, Lim (e^x-1)", answer:"x" },
-                { prompt:"per x->0, Lim arctan(x)", answer:"x" },
-                { prompt:"per x->0, Lim arcsin(x)", answer:"x" },
+                { prompt:"per x->0, Lim tan(x)/x", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} { \\tan (x) \\over x }",prompt:"uguale a"}, answer:"1" },
+                { prompt:"per x->0, Lim (e^x-1)/x", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} { e^x - 1 \\over x }",prompt:"uguale a"}, answer:"1" },
+                { prompt:"per x->0, Lim arctan(x)/x", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} { \\arctan x \\over x }",prompt:"uguale a"}, answer:"1" },
+                { prompt:"per x->0, Lim arcsin(x)/x", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} { \\arcsin x \\over x }",prompt:"uguale a"}, answer:"1" },
 
-                { prompt:"per x->0+, Lim xln(x)", answer:"0"},
-                { prompt:"per x->0, Lim (a^x-1)/x", answer:["ln(a)","log(a)"]},
-                { prompt:"per x->0, Lim (1-cos(x))/x^2", answer:"1/2"},
+                { prompt:"per x->0, Lim sin(x)", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} \\sin x",prompt:"uguale a"}, answer:"x" },
+                { prompt:"per x->0, Lim ln(1+x)", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} \\ln (1+x)",prompt:"uguale a"},answer:"x" },
+                { prompt:"per x->0, Lim tan(x)", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} \\tan x",prompt:"uguale a"}, answer:"x" },
+                { prompt:"per x->0, Lim (e^x-1)", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} e^x - 1",prompt:"uguale a"}, answer:"x" },
+                { prompt:"per x->0, Lim arctan(x)", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} \\arctan x",prompt:"uguale a"}, answer:"x" },
+                { prompt:"per x->0, Lim arcsin(x)", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} \\arcsin x",prompt:"uguale a"}, answer:"x" },
 
-                { prompt:"per x->infinito, Lim (1+1/x)^x", answer:"e" },
-                { prompt:"per x->infinito, Lim (1+x)^-x", answer:"e" },
+                { prompt:"per x->0+, Lim xln(x)", promptMathJax:{equation:"\\lim_{x \\rightarrow 0^+} x \\ln x",prompt:"uguale a"}, answer:"0"},
+                { prompt:"per x->0, Lim (a^x-1)/x", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} {a^x - 1 \\over x}",prompt:"uguale a"}, answer:["ln(a)","log(a)"]},
+                { prompt:"per x->0, Lim (1-cos(x))/x^2", promptMathJax:{equation:"\\lim_{x \\rightarrow 0} {1 - \\cos x \\over x^2}",prompt:"uguale a"}, answer:"1/2"},
 
-                { prompt:"per x->infinito, Lim (1+a/x)^x", answer:"e^a" },
+                { prompt:"per x->infinito, Lim (1+1/x)^x", promptMathJax:{equation:"\\lim_{x \\rightarrow \\infty} (1 + {1 \\over x})^x",prompt:"uguale a"}, answer:"e" },
+                { prompt:"per x->infinito, Lim (1+x)^-x", promptMathJax:{equation:"\\lim_{x \\rightarrow \\infty} (1 + x)^{-x}",prompt:"uguale a"}, answer:"e" },
+
+                { prompt:"per x->infinito, Lim (1+a/x)^x", promptMathJax:{equation:"\\lim_{x \\rightarrow \\infty} (1 + {a \\over x})^x",prompt:"uguale a"}, answer:"e^a" },
             ]
         },
         {
         category:"Serie note",
             questions:[
-                { prompt:"Σq^n, diverge per q", promptMathJax:["\\sum_{n=1}^{\\infty} q^n","diverge per q"], answer:"q≥1" },
-                { prompt:"Σq^n, converge per q", promptMathJax:["\\sum_{n=1}^{\\infty} q^n","converge per q"],answer:"-1<q<1" },
-                { prompt:"Σq^n, a cosa converge se -1<q<1", promptMathJax:["\\sum_{n=1}^{\\infty} q^n","se -1<q<1, a cosa converge"], answer:"1/(1-q)" },
-                { prompt:"Σq^n, è indeterminata per q", promptMathJax:["\\sum_{n=1}^{\\infty} q^n","è indeterminata per q"], answer:"q≤-1" },
-                { prompt:"Σ1/n^a diverge per a", answer:"a≤1" },
-                { prompt:"Σ1/n(ln(n))^a diverge per a", answer:"a≤1" },
-                { prompt:"Σ1/n^a converge per a", answer:"a>1" },
-                { prompt:"Σ1/n(ln(n))^a converge per a", answer:"a>1" },
+                { prompt:"Σq^n, diverge per q", promptMathJax:{equation:"\\sum_{n=0}^{\\infty} q^n",prompt:"diverge per q"}, answer:"q≥1" },
+                { prompt:"Σq^n, converge per q", promptMathJax:{equation:"\\sum_{n=0}^{\\infty} q^n",prompt:"converge per q"},answer:"-1<q<1" },
+                { prompt:"Σq^n, a cosa converge se -1<q<1", promptMathJax:{equation:"\\sum_{n=0}^{\\infty} q^n",prompt:"se -1<q<1, a cosa converge"}, answer:"1/(1-q)" },
+                { prompt:"Σq^n, è indeterminata per q", promptMathJax:{equation:"\\sum_{n=0}^{\\infty} q^n",prompt:"è indeterminata per q"}, answer:"q≤-1" },
+                { prompt:"Σ1/n^a diverge per a", promptMathJax:{equation:"\\sum_{n=1}^{\\infty} {1 \\over n^a}",prompt:"diverge per a"}, answer:"a≤1" },
+                { prompt:"Σ1/n^a converge per a", promptMathJax:{equation:"\\sum_{n=1}^{\\infty} {1 \\over n^a}",prompt:"converge per a"}, answer:"a>1" },
+                { prompt:"Σ1/n(ln(n))^a diverge per a", promptMathJax:{equation:"\\sum_{n=1}^{\\infty} {1 \\over n (\\ln n)^a}",prompt:"diverge per a"}, answer:"a≤1" },
+                { prompt:"Σ1/n(ln(n))^a converge per a", promptMathJax:{equation:"\\sum_{n=1}^{\\infty} {1 \\over n (\\ln n)^a}",prompt:"converge per a"}, answer:"a>1" },
             ]
         },
         {
         category:"Scomposizioni notevoli",
             questions:[
-                { prompt:"a^2 - b^2", answer:["(a+b)(a-b)","(a-b)(a+b)"] },
-                { prompt:"a^2 + b^2", answer:["(a+b)^2-2ab"] },
-                { prompt:"a^3 - b^3", answer:["(a-b)(a^2+ab+b^2)","(a^2+ab+b^2)(a-b)"] },
-                { prompt:"a^3 + b^3", answer:["(a+b)(a^2-ab+b^2)","(a^2-ab+b^2)(a+b)"] },
+                { prompt:"a^2 - b^2", promptMathJax:{equation:"a^2-b^2",prompt:"si scompone in"}, answer:["(a+b)(a-b)","(a-b)(a+b)"] },
+                { prompt:"a^2 + b^2", promptMathJax:{equation:"a^2+b^2",prompt:"si scompone in"}, answer:["(a+b)^2-2ab"] },
+                { prompt:"a^3 - b^3", promptMathJax:{equation:"a^3-b^3",prompt:"si scompone in"}, answer:["(a-b)(a^2+ab+b^2)","(a^2+ab+b^2)(a-b)"] },
+                { prompt:"a^3 + b^3", promptMathJax:{equation:"a^3+b^3",prompt:"si scompone in"}, answer:["(a+b)(a^2-ab+b^2)","(a^2-ab+b^2)(a+b)"] },
             ]
         },
     ];
@@ -167,6 +167,7 @@ window.addEventListener("load", () => {
 
     function pickQuestion() {
         let categoryGuess = ~~(Math.random() * categories.length);
+        _input.value = "";
         while (categoryGuess===_categoryIndex && categories.length>1) {
             categoryGuess = ~~(Math.random() * categories.length);
         }
@@ -191,15 +192,11 @@ window.addEventListener("load", () => {
     function answerQuestion() {
         const correctAnswer = categories[_categoryIndex].questions[_questionIndex].answer,
             userAnswer = _input.value.toLowerCase();
-        if (userAnswer==="update") {
-            updateQuestionUI();
-        }
-        else if (userAnswer==="skip") {
+        if (userAnswer==="skip"||userAnswer==="s") {
             pickQuestion();
         }
         else if ((typeof correctAnswer==="string"&&correctAnswer===userAnswer)||Array.isArray(correctAnswer)&&correctAnswer.includes(userAnswer)) {
             pickQuestion();
-            _input.value = "";
             _inputTitle.textContent = "Risposta";
         }
         else {
@@ -215,20 +212,20 @@ window.addEventListener("load", () => {
         let {prompt:_promptText,promptMathJax:_promptMathJax,answer} = cat.questions[_questionIndex];
 
         _inputTitle.textContent = "Risposta";
-        if (_promptMathJax&&Array.isArray(_promptMathJax)) {
+        if (_promptMathJax) {
             try {
                 /** @ts-ignore */
-                _prompt.innerHTML = MathJax.tex2mml(_promptMathJax[0], {}) + "<p>" + _promptMathJax[1] + (typeof answer === "string" ? " = ?" : " =* ?") + "</p>";
+                _prompt.innerHTML = MathJax.tex2mml(_promptMathJax.equation, {}) 
+                    + "<p>" + _promptMathJax.prompt.replaceAll("<","&lt").replaceAll(">","&gt") + (typeof answer === "string" ? " = ?" : " =* ?") + "</p>";
             }
             catch(e) {
-                console.log(e);
                 setTimeout(updateQuestionUI,100);
             }
         }
         else {
-        _prompt.textContent = 
-                _promptText
-                + (typeof answer==="string"?" = ?" : " =* ?");
+            _prompt.textContent = 
+                    _promptText
+                    + (typeof answer==="string"?" = ?" : " =* ?");
         }
     }
 
